ea8b061517ee416d07f54214d1c550a122c0afc8
seoyeon <seoyeon653131@gmail.com>
2023-05-29 22:31:26 +0900

    과제 2-3 제출합니다

--- asg2/src/Detail.js
+++ asg2/src/Detail.js
@@ -11,10 +11,11 @@ function Detail() {
 
   let albumTitle, albumSubtitle, albumPrice, albumImage;
 
+   // 앨범 ID에 따라 앨범 정보 할당 , 2만원으로 통일
   if (albumId === 'love-poem') {
     albumTitle = 'Love poem';
     albumSubtitle = 'The 5th Mini Album';
-    albumPrice = '14,000원';
+    albumPrice = '20,000원';
     albumImage = LovePoem;
 
   } else if (albumId === 'lilac') {
@@ -26,22 +27,23 @@ function Detail() {
   } else if (albumId === 'palette') {
     albumTitle = 'Palette';
     albumSubtitle = 'The 4th Album';
-    albumPrice = '16,000원';
+    albumPrice = '20,000원';
     albumImage = Palette;
   }
-
-  albumPrice = '20,000원'; //가격을 2만원으로 통일
 
   // 가격을 숫자로 변환
   const originalPrice = parseInt(albumPrice.replace(/[^0-9]/g, ''));
 
   const [showSaleAlert, setShowSaleAlert] = useState(false);
 
+  
   useEffect(() => {
+    // 3초 후에 할인 알림 창을 사라지게 설정
     const timer = setTimeout(() => {
       setShowSaleAlert(false);
     }, 3000);
 
+    // 컴포넌트가 언마운트될 때 타이머 클리어
     return () => clearTimeout(timer);
   }, []);
 
