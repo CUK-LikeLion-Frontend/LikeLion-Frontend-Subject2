21b3bde74bca87aa33894fd5c26cbf91b81a57c6
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-05-29 14:20:52 +0900

    언제 나눠서 pr하지...

--- homepage/src/components/Layout/Footer.jsx
+++ homepage/src/components/Layout/Footer.jsx
@@ -6,7 +6,15 @@ function Footer() {
     <Container>
       <footer className="py-4 my-5 border-top">
         <p className="text-muted text-center font-weight-bold">
-          © 2023 가톨릭대학교 멋쟁이 사자처럼
+          ©{" "}
+          <a
+            class="text-decoration-none text-black"
+            target="_blank"
+            rel="noreferrer"
+            href="https://likelioncuk.notion.site/likelioncuk/ade52330317b445998c7b066071bbff4"
+          >
+            2023 가톨릭대학교 멋쟁이 사자처럼
+          </a>
         </p>
       </footer>
     </Container>
