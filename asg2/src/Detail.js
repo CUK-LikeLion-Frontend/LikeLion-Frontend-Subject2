6ee0b4a14011d1d8017c01614ac520a3577509d8
seoyeon <seoyeon653131@gmail.com>
2023-05-31 14:13:07 +0900

    Feat: 과제 2-3 제출합니다

--- asg2/src/Detail.js
+++ asg2/src/Detail.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Grid, Button, Alert, Box } from '@mui/material';
+import { Container, Row, Col, Button, Alert } from 'react-bootstrap';
 
 import LovePoem from "../src/img/LovePoem.jpg";
 import Lilac from "../src/img/Lilac.jpg";
@@ -11,7 +11,7 @@ function Detail() {
 
   let albumTitle, albumSubtitle, albumPrice, albumImage;
 
-   // 앨범 ID에 따라 앨범 정보 할당 , 2만원으로 통일
+  // 앨범 ID에 따라 앨범 정보 할당 , 2만원으로 통일
   if (albumId === 'love-poem') {
     albumTitle = 'Love poem';
     albumSubtitle = 'The 5th Mini Album';
@@ -33,10 +33,8 @@ function Detail() {
 
   // 가격을 숫자로 변환
   const originalPrice = parseInt(albumPrice.replace(/[^0-9]/g, ''));
-
   const [showSaleAlert, setShowSaleAlert] = useState(false);
 
-  
   useEffect(() => {
     // 3초 후에 할인 알림 창을 사라지게 설정
     const timer = setTimeout(() => {
@@ -53,34 +51,36 @@ function Detail() {
 
   return (
     <div className="detail">
-      <Grid container spacing={5} alignItems="flex-start">
-        <Grid item>
-          <img src={albumImage} alt={albumTitle} style={{ width: '100%' }} />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <div style={{ marginTop: '10px' }}>
-            <h2 style={{ textAlign: 'left', marginBottom: '5px' }}>{albumTitle}</h2>
-            <p style={{ textAlign: 'left', marginBottom: '5px' }}>{albumSubtitle}</p>
-            <p style={{ textAlign: 'left', marginBottom: '5px' }}>
-              Price: {showSaleAlert ? `${originalPrice * 0.7}원` : albumPrice}
-            </p>
-            <Button onClick={handlePurchase} disabled={showSaleAlert}>
-              주문하기
-            </Button>
-            {showSaleAlert && (
-              <Box sx={{ marginTop: '10px' }}>
-                <Alert
-                  severity="warning"
-                  onClose={() => setShowSaleAlert(false)}
-                  sx={{ backgroundColor: 'lightorange', color: 'black' }}
-                >
-                  3초 안에 구매하면 30% 세일 중! 가격: {originalPrice * 0.7}원
-                </Alert>
-              </Box>
-            )}
-          </div>
-        </Grid>
-      </Grid>
+      <Container>
+        {showSaleAlert && (
+          <Alert
+            variant="warning"
+            onClose={() => setShowSaleAlert(false)}
+            dismissible
+            className="mt-3"
+            style={{ backgroundColor: 'lightorange', color: 'black' }}
+          >
+            3초 안에 구매하면 30% 세일 중! 가격: {originalPrice * 0.7}원
+          </Alert>
+        )}
+        <Row>
+          <Col>
+            <img src={albumImage} alt={albumTitle} style={{ width: '100%' }} />
+          </Col>
+          <Col xs={12} sm={6} md={4}>
+            <div style={{ marginTop: '10px' }}>
+              <h2 style={{ textAlign: 'left', marginBottom: '5px' }}>{albumTitle}</h2>
+              <p style={{ textAlign: 'left', marginBottom: '5px' }}>{albumSubtitle}</p>
+              <p style={{ textAlign: 'left', marginBottom: '5px' }}>
+                Price: {showSaleAlert ? `${originalPrice * 0.7}원` : albumPrice}
+              </p>
+              <Button onClick={handlePurchase} disabled={showSaleAlert}>
+                주문하기
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
