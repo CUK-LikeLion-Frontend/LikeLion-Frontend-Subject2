573e219c9f7bc75f2a27f7670f275e9de1431811
Seo Eun <seo606101@naver.com>
2023-05-31 19:37:44 +0900

    feat : 과제2-2 구현

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Footer from "./Layout/footer";
 import NavBar from "./NavBar";
 import { Routes, Route, Link } from "react-router-dom";
+import Albums from "./Album";
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
         <Route path="/" element={<div className="main-bg" />} />
+        <Route path="/albums" element={<Albums />} />
       </Routes>
       <Footer />
     </>
