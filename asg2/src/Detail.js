dce9b575784e86f11aa285ff0defb468a853657e
seoyeon <seoyeon653131@gmail.com>
2023-05-31 16:34:33 +0900

    Feat: 과제 2-4 제출합니다

--- asg2/src/Detail.js
+++ asg2/src/Detail.js
@@ -11,19 +11,16 @@ function Detail() {
 
   let albumTitle, albumSubtitle, albumPrice, albumImage;
 
-  // 앨범 ID에 따라 앨범 정보 할당 , 2만원으로 통일
   if (albumId === 'love-poem') {
     albumTitle = 'Love poem';
     albumSubtitle = 'The 5th Mini Album';
     albumPrice = '20,000원';
     albumImage = LovePoem;
-
   } else if (albumId === 'lilac') {
     albumTitle = 'LILAC';
     albumSubtitle = 'The 5th Album';
     albumPrice = '20,000원';
     albumImage = Lilac;
-
   } else if (albumId === 'palette') {
     albumTitle = 'Palette';
     albumSubtitle = 'The 4th Album';
@@ -31,41 +28,31 @@ function Detail() {
     albumImage = Palette;
   }
 
-  // 가격을 숫자로 변환
   const originalPrice = parseInt(albumPrice.replace(/[^0-9]/g, ''));
-  const [showSaleAlert, setShowSaleAlert] = useState(false);
+  const [showSaleAlert, setShowSaleAlert] = useState(true);
 
   useEffect(() => {
-    // 3초 후에 할인 알림 창을 사라지게 설정
     const timer = setTimeout(() => {
       setShowSaleAlert(false);
     }, 3000);
 
-    // 컴포넌트가 언마운트될 때 타이머 클리어
     return () => clearTimeout(timer);
   }, []);
 
-  const handlePurchase = () => {
-    setShowSaleAlert(true);
-  };
 
   return (
     <div className="detail">
       <Container>
-        {showSaleAlert && (
           <Alert
             variant="warning"
-            onClose={() => setShowSaleAlert(false)}
-            dismissible
-            className="mt-3"
+            show={showSaleAlert}s
             style={{ backgroundColor: 'lightorange', color: 'black' }}
           >
             3초 안에 구매하면 30% 세일 중! 가격: {originalPrice * 0.7}원
           </Alert>
-        )}
         <Row>
           <Col>
-            <img src={albumImage} alt={albumTitle} style={{ width: '100%' }} />
+            <img src={`${process.env.PUBLIC_URL}/img/${albumTitle}.jpg`} alt={albumTitle} style={{ width: '100%' }} />
           </Col>
           <Col xs={12} sm={6} md={4}>
             <div style={{ marginTop: '10px' }}>
@@ -74,7 +61,7 @@ function Detail() {
               <p style={{ textAlign: 'left', marginBottom: '5px' }}>
                 Price: {showSaleAlert ? `${originalPrice * 0.7}원` : albumPrice}
               </p>
-              <Button onClick={handlePurchase} disabled={showSaleAlert}>
+              <Button>
                 주문하기
               </Button>
             </div>
