64463a5590a877dca11c17e53a0032cdb28aa060
Seo Eun <seo606101@naver.com>
2023-05-31 19:43:51 +0900

    feat : 과제 2-3

--- shop/src/App.js
+++ shop/src/App.js
@@ -3,6 +3,7 @@ import Footer from "./Layout/footer";
 import NavBar from "./NavBar";
 import { Routes, Route, Link } from "react-router-dom";
 import Albums from "./Album";
+import Detail from "./Detail";
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<div className="main-bg" />} />
         <Route path="/albums" element={<Albums />} />
+        <Route path="/detail/:name" element={<Detail />} />
       </Routes>
       <Footer />
     </>
